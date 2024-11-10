6ff4ec500c5f04430e662471317df2ad46c21106
Brent Dalling <brentdalling@icloud.com>
2024-11-10 14:04:19 -0700

    remove the alert when videos start playing

--- src/AppFrame.js
+++ src/AppFrame.js
@@ -3,25 +3,8 @@ import TopBar from "./components/TopBar/TopBar";
 import BottomBar from "./components/BottomBar/BottomBar";
 import Canvas from "./components/Canvas/Canvas";
 
-import client from "socket.io-client";
-import { useEffect } from "react";
-
 function AppFrame() {
 
-  useEffect(() => {
-    const websocket = window.checkIfAPITargetIsPresent();
-
-    const socket = client(websocket);
-
-    socket.on("connect", () => {
-      console.log("Connected to the socket API.");
-
-      socket.on("video:start", () => {
-        alert("video Starting");
-      });
-    });
-  });
-  
   return (
     <>
       <TopBar />
