b25bfdfa7a5994cddfb9345d7d0b53557f4fa441
Brent Dalling <brent.dalling@timelydevs.com>
2025-07-10 20:42:25 -0600

    fix some build issues on coolify

--- phaser-app/src/main.js
+++ phaser-app/src/main.js
@@ -7,7 +7,7 @@ import './plugins/rexbarreldistortionpipelineplugin.min.js';
 
 // Log environment configuration
 console.log('Environment: ', {
-    wsServerUrl: import.meta.env.VITE_WS_SERVER_URL || 'ws://localhost:8081',
+    wsServerUrl: import.meta.env.VITE_WS_SERVER_URL || 'ws://websocket-server:8081',
     mode: import.meta.env.MODE
 });
 
